b8a961b3bc9db0c986d920694d3e3ab46b5f3d7d
RahulVerma05 <106341333+RahulVerma05@users.noreply.github.com>
2024-02-14 16:46:33 +0530

    Update 3_WEATHER.py

--- SQL/HackerRank/3_WEATHER.py
+++ SQL/HackerRank/3_WEATHER.py
@@ -22,3 +22,19 @@ Query the list of CITY names from STATION that do not start with vowels. Your re
 SELECT DISTINCT CITY
 FROM STATION
 WHERE LEFT(CITY,1) NOT IN ('a','e','i','o','u')
+
+'''
+4.
+Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+'''
+SELECT DISTINCT CITY
+FROM STATION
+WHERE RIGHT(CITY,1) NOT IN ('a','e','i','o','u')
+
+'''
+5.
+Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+'''
+SELECT DISTINCT CITY 
+FROM STATION
+WHERE LEFT(CITY,1) NOT IN ('a','e','i','o','u') OR RIGHT(CITY,1) NOT IN ('a','e','i','o','u') 
