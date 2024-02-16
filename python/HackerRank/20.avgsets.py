0751aa9c4124032cfdde777f376d1232194b0f88
RahulVerma05 <106341333+RahulVerma05@users.noreply.github.com>
2024-02-16 17:56:48 +0530

    Create 20.avgsets.py

---
+++ python/HackerRank/20.avgsets.py
@@ -0,0 +1,11 @@
+'''
+Now, let's use our knowledge of sets and help Mickey.
+
+Ms. Gabriel Williams is a botany professor at District College. One day, she asked her student Mickey to compute the average of all the plants with distinct heights in her greenhouse.
+'''
+def average(array):
+    array = set(array)
+    s = sum(array)
+    n = len(array)
+    avg = float(s/n)
+    return avg
