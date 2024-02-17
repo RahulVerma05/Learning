f35e5209af621958a75ee44f5a36f9a3053fce80
RahulVerma05 <106341333+RahulVerma05@users.noreply.github.com>
2024-02-17 10:54:49 +0530

    Update 6.station.sql

--- SQL/HackerRank/6.station.sql
+++ SQL/HackerRank/6.station.sql
@@ -20,3 +20,9 @@ Query the total population of all cities in CITY where District is California.
 select sum(population)
 from city
 where district = 'California'
+
+'''
+Query the average population for all cities in CITY, rounded down to the nearest integer.
+'''
+select round(avg(population),0)
+from city
