89833e0a26ec24e07bb982d57a89a805aee97d1a
RahulVerma05 <106341333+RahulVerma05@users.noreply.github.com>
2024-02-17 10:59:45 +0530

    Update 6.station.sql

--- SQL/HackerRank/6.station.sql
+++ SQL/HackerRank/6.station.sql
@@ -26,3 +26,10 @@ Query the average population for all cities in CITY, rounded down to the nearest
 '''
 select round(avg(population),0)
 from city
+
+'''
+Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than  and less than . Truncate your answer to  decimal places.
+'''
+select round(sum(LAT_N),4)
+from station
+where LAT_N > 38.7880 and LAT_N < 137.2345
