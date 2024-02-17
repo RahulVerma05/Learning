fca1c76ab0bae1a05fa2714eb24132ea5119ef4a
RahulVerma05 <106341333+RahulVerma05@users.noreply.github.com>
2024-02-17 10:28:37 +0530

    Update 6.station.sql

--- SQL/HackerRank/6.station.sql
+++ SQL/HackerRank/6.station.sql
@@ -6,3 +6,10 @@ The sum of all values in LONG_W rounded to a scale of  decimal places
 '''
 select  round(sum(LAT_N),2), round(sum(LONG_W),2)
 from Station
+
+'''
+Query a count of the number of cities in CITY having a Population larger than 100000.
+'''
+select count(name)
+from city
+where Population > 100000
