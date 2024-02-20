1e4aeda036a04914ccba952705ba12b9b4839ba5
RahulVerma05 <106341333+RahulVerma05@users.noreply.github.com>
2024-02-20 16:51:11 +0530

    Update 7.baseic.sql

--- SQL/HackerRank/7.baseic.sql
+++ SQL/HackerRank/7.baseic.sql
@@ -14,3 +14,17 @@ where LAT_N = (select max(LAT_N)
                from station 
                where LAT_N< 137.2345)
 '''
+Query the smallest Northern Latitude (LAT_N) from STATION that is greater than . Round your answer to  decimal places.
+'''
+select round(min(LAT_N),4)
+from station
+where LAT_N > 38.7780
+
+'''
+Query the Western Longitude (LONG_W)where the smallest Northern Latitude (LAT_N) in STATION is greater than . Round your answer to  decimal places.
+'''
+select round(LONG_W,4)
+from station
+where LAT_N = (select min(LAT_N)
+              from station
+              where LAt_N > 38.7780)
