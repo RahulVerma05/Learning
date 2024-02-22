98c3a90a225c8125ee01c7d4c74ec284b0dea292
RahulVerma05 <106341333+RahulVerma05@users.noreply.github.com>
2024-02-22 10:45:07 +0530

    Update 9.aggregation.sql

--- SQL/HackerRank/9.aggregation.sql
+++ SQL/HackerRank/9.aggregation.sql
@@ -4,3 +4,12 @@ Query the average population of all cities in CITY where District is California.
 select avg(population)
 from city
 where district = 'california'
+
+'''
+Query the sum of the populations for all Japanese cities in CITY. The COUNTRYCODE for Japan is JPN.
+'''
+select sum(population)
+from city
+where countrycode = 'JPN'
+
+'''
