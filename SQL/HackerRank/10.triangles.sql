6f2d6e9908ad2415822f603b790ca64e222f70dc
RahulVerma05 <106341333+RahulVerma05@users.noreply.github.com>
2024-02-23 17:27:49 +0530

    Update 10.triangles.sql

--- SQL/HackerRank/10.triangles.sql
+++ SQL/HackerRank/10.triangles.sql
@@ -15,3 +15,12 @@ when A=B or A=C or B=C then 'Isosceles'
 else 'Scalene' end
 else 'Not A Triangle' end
 from Triangles
+
+/* 
+Samantha was tasked with calculating the average monthly salaries for all employees in the EMPLOYEES table, but did not realize her keyboard's  key was broken until after completing the calculation. She wants your help finding the difference between her miscalculation (using salaries with any zeros removed), and the actual average salary.
+
+Write a query calculating the amount of error (i.e.:  average monthly salaries), and round it up to the next integer.
+*/
+
+select ceil(avg(salary) - avg(replace(salary,'0','')))
+from employees
