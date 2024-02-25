4e854147176eb3bfc39e3f93d17f6b1d0dc4089a
RahulVerma05 <106341333+RahulVerma05@users.noreply.github.com>
2024-02-25 12:16:03 +0530

    Create 12.binarytree.sql

---
+++ SQL/HackerRank/12.binarytree.sql
@@ -0,0 +1,19 @@
+/*
+You are given a table, BST, containing two columns: N and P, where N represents the value of a node in Binary Tree, and P is the parent of N.
+
+
+
+Write a query to find the node type of Binary Tree ordered by the value of the node. Output one of the following for each node:
+
+Root: If node is root node.
+Leaf: If node is leaf node.
+Inner: If node is neither root nor leaf node.
+*/
+Select N,
+case 
+    when P is null then "Root"
+    when N IN (select P from BST) then "Inner"
+    else "Leaf"
+end as type_
+from BST 
+order by N
